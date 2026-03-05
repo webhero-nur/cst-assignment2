55834bf7765517f5e44123074d4a1ab5a76972f7
MD Nure Alom <86778013+webhero-nur@users.noreply.github.com>
2026-03-05 22:09:54 +0600

    Navbar added

---
+++ src/Navbar/Navbar.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+const Navbar = () => {
+    const links = (
+        <>
+            <li>
+                <a>Home</a>
+            </li>
+            <li>
+                <a>FAQ</a>
+            </li>
+            <li>
+                <a>Changelog</a>
+            </li>
+            <li>
+                <a>Blog</a>
+            </li>
+            <li>
+                <a>Download</a>
+            </li>
+            <li>
+                <a>Contact</a>
+            </li>
+            <button className="btn ms-4 bg-linear-to-r from-purple-700 to-purple-500 text-white">
+                + New Ticket
+            </button>
+        </>
+    );
+    return (
+        <div className="navbar bg-base-100 shadow-sm max-w-7xl mx-auto">
+            <div className="flex-1">
+                <a className="font-bold text-xl">CS - Ticket System</a>
+            </div>
+            <div className="flex-none">
+                <ul className="menu hidden md:flex menu-horizontal px-1 items-center">
+                    {links}
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
